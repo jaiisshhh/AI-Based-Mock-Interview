2446e143446a318246cf162155eb5050119ee890
Sanskar Gupta <sanskargupta1404@gmail.com>
2025-08-02 20:41:53 +0530

    Cleark Auth Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { PublicLayout } from "@/layouts/public-layout";
+import AuthenticationLayout from "@/layouts/auth-layout";
+import ProtectedRoutes from "@/layouts/protected-routes";
+import { MainLayout } from "@/layouts/main-layout";
+
+import HomePage from "@/routes/home";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+
 const App = () => {
   return (
-    <div className="text-blue-500">App</div>
-  )
-}
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
 
-export default App
+        {/* Authentication Layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* Protected routes */}
+        <Route
+          element={
+            <ProtectedRoutes>
+              <MainLayout />
+            </ProtectedRoutes>
+          }
+        >
+          {/* Add all the protect routes */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
