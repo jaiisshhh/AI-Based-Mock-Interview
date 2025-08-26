bfeabb59c13662ed3c347247752fb3bf202d1db7
Sanskar Gupta <sanskargupta1404@gmail.com>
2025-08-27 00:14:02 +0530

    Nearing Completion

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -117,3 +117,5 @@ export const Footer = () => {
     </div>
   );
 };
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from "@clerk/clerk-react";
 
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import App from "./App.tsx";
+import { ToasterProvider } from "./provider/toast-provider.tsx";
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error("Missing Publishable Key");
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
+      <App />
+      <ToasterProvider />
     </ClerkProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
